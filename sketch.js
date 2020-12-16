148b3259b393e1757c7b314ff4ae0767d9304800
Saumil251 <72085873+Saumil251@users.noreply.github.com>
2020-12-16 20:12:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var gameState=0,playerCount,database,form,player,game,canvas;
+function setup(){
+  canvas=createCanvas(400,400)
+  database=firebase.database()
+  game=new Game()
+  game.getState()
+  game.start()
+}
+function draw(){
+  
+}
